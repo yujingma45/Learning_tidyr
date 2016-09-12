8067f8484ed4a0a83ce583bb6be160e7451bdd2a
yujingma45 <yujing.ma45@gmail.com>
2016-09-12 18:13:45 -0400

    adding notes

---
+++ DataTidyingWithTidyr.R
@@ -0,0 +1,7 @@
+
+# Data Tidying with tidyr --------------------------------------------------
+
+## Prerequisites
+install.packages(c("tidyr","devtools"))
+devtools::install_github("garrettgman/DSR")
+# Note: DSR is the dataset
